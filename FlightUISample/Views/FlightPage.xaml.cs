1a1acdc2fdfa10249c3b54470a52d1c8f9428b34
hp <hp@DESKTOP-FKTABFC>
2020-05-30 22:48:13 +0300

    a commit

---
+++ FlightUISample/Views/FlightPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using FlightUISample.ViewModels;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FlightUISample.Views
+{
+    public partial class FlightPage : ContentPage
+    {
+        public FlightPage()
+        {
+            InitializeComponent();
+            BindingContext = new FlightViewModel();
+        }
+    }
+}
